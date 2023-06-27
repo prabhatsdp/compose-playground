4e2f36dd17f2f27d9de35b6d1560c099fef66a25
Prabhat Pandey <prabhatsdp@gmail.com>
2023-06-27 18:33:16 +0530

    colors changed

--- app/src/main/java/dev/prabhatpandey/composeplayground/core/ui/theme/Theme.kt
+++ app/src/main/java/dev/prabhatpandey/composeplayground/core/ui/theme/Theme.kt
@@ -10,22 +10,36 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green60,
+    secondary = Gray90,
+    tertiary = SkyBlue60,
+    background = Blue20,
+    surface = Blue30,
+    onSurface = Gray90,
+    onBackground = Gray90,
+    onTertiary = Color.White,
+    onPrimary = Blue20,
+    onSecondary = Blue20
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    primary = Green60,
+    secondary = Blue20,
+    tertiary = SkyBlue60,
+    background = Color.White,
+    surface = Gray90,
+    onSurface = Blue20,
+    onBackground = Blue20,
+    onTertiary = Color.White,
+    onPrimary = Gray90,
+    onSecondary = Gray90
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -41,7 +55,7 @@ private val LightColorScheme = lightColorScheme(
 fun ComposePlaygroundTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
