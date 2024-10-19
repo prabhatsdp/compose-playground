ee028f9c1f745314685e22df92e45d9cb33a341c
Prabhat Pandey <prabhatsdp@gmail.com>
2024-10-19 08:14:47 +0530

    Add under construction label in swipe calendar

--- app/src/main/java/dev/prabhatpandey/composeplayground/features/calendar/ui/SwipingCalendarScreen.kt
+++ app/src/main/java/dev/prabhatpandey/composeplayground/features/calendar/ui/SwipingCalendarScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 /**
@@ -25,8 +26,7 @@ fun SwipingCalendarScreen() {
                 .fillMaxSize()
                 .padding(it)
         ) {
-            Text(text = "Swiping calendar")
-
+            Text(text = "Under Construction", modifier = Modifier.align(Alignment.Center))
         }
     }
 }
